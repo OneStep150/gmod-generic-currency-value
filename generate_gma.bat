0b76c81ad16f49800b9f0fd615203b2f3b6ffdab
Timo <scrilla@freenet.de>
2020-07-13 19:57:47 +0200

    Made updating easier

---
+++ generate_gma.bat
@@ -0,0 +1,2 @@
+"F:\Program Files (x86)\Steam\steamapps\common\garrysmod\bin\gmad.exe" create -folder "F:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\generic_currency_value" -out "F:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\generic_currency_value\generic_currency_value.gma"
+pause
