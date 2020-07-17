f2cb2b67f870d87d52065ce25a14b087921eb355
Timo <scrilla@freenet.de>
2020-07-17 23:46:13 +0200

    Some adjustments to the way obj gets drawn

--- lua/entities/gcv_obj_retrievepackage/cl_init.lua
+++ lua/entities/gcv_obj_retrievepackage/cl_init.lua
@@ -5,13 +5,17 @@ CV.CL.ENT = CV.CL.ENT or {}
 CV.CL.ENT.Objective = CV.CL.ENT.Objective or {}
 CV.CL.ENT.Objective.RetrievePackage = CV.CL.ENT.Objective.RetrievePackage or {}
 
+CV.CL.ENT.Objective.RetrievePackage.TextPosition = function(ent)
+  return ent:GetPos() + Vector(0, 0, 50)
+end
+
 CV.CL.ENT.Objective.RetrievePackage.TextScale = function(ent)
   local distanceToLocalPlayer = LocalPlayer():GetPos():DistToSqr(ent:GetPos())
   return math.Clamp(1 + distanceToLocalPlayer / 2500000, 0, 20 )
 end
 
 CV.CL.ENT.Objective.RetrievePackage.TextAngle = function(ent)
-  local targetAngle = (LocalPlayer():GetPos() - ent:GetPos()):Angle()
+  local targetAngle = ((LocalPlayer():GetPos() + Vector(0, 0, 20)) - ent:GetPos()):Angle()
   return Angle(0, 90 + targetAngle.yaw, 90 + targetAngle.pitch)
 end
 
@@ -20,7 +24,7 @@ function ENT:Draw()
 
    if GetConVar("gcv_objective_drawpos"):GetBool() then
      cam.IgnoreZ(true)
-     cam.Start3D2D( self:GetPos(), CV.CL.ENT.Objective.RetrievePackage.TextAngle(self), CV.CL.ENT.Objective.RetrievePackage.TextScale(self))
+     cam.Start3D2D(CV.CL.ENT.Objective.RetrievePackage.TextPosition(self), CV.CL.ENT.Objective.RetrievePackage.TextAngle(self), CV.CL.ENT.Objective.RetrievePackage.TextScale(self))
          draw.SimpleTextOutlined( "Retrieve!", "DermaDefault", 0, 0, Color(255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0, 0, 0) )
      cam.End3D2D()
    end
@@ -28,7 +32,7 @@ end
 
 CV.CL.ENT.Objective.RetrievePackage.AddHalo = function()
   if GetConVar("gcv_objective_drawpos"):GetBool() then
-     halo.Add( ents.FindByClass( "gcv_obj_retrievepackage" ), Color( 255, 0, 0 ), 5, 5, 2, false, true )
+     halo.Add( ents.FindByClass( "gcv_obj_retrievepackage" ), Color( 255, 0, 0 ), 5, 5, 2, true, true )
   end
 end
 
