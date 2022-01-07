219b3d04fc2e74bdd650171b549500177a835bb6
DESKTOP-0LQ89AE\Admin <scrilla@freenet.de>
2022-01-07 19:11:43 +0100

    Updated Update Scripts

--- generate_gma.bat
+++ generate_gma.bat
@@ -1,2 +1,2 @@
-"F:\Program Files (x86)\Steam\steamapps\common\garrysmod\bin\gmad.exe" create -folder "F:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\generic_currency_value" -out "F:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\generic_currency_value\generic_currency_value.gma"
+"D:\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder "D:\Steam\steamapps\common\GarrysMod\garrysmod\addons\generic_currency_value" -out "D:\Steam\steamapps\common\GarrysMod\garrysmod\addons\generic_currency_value\generic_currency_value.gma"
 pause
--- update_workshop.bat
+++ update_workshop.bat
@@ -1,2 +1,2 @@
-"F:\Program Files (x86)\Steam\steamapps\common\garrysmod\bin\gmpublish.exe" update -addon "F:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons\generic_currency_value\generic_currency_value.gma" -id "2164922201" -changes "please refer to github for details about changes"
+"D:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -addon "D:\Steam\steamapps\common\GarrysMod\garrysmod\addons\generic_currency_value\generic_currency_value.gma" -id "2164922201" -changes "please refer to github for details about changes"
 pause
